afb9f159308f68a748e36f686d6fbfbc5199ae1e
tsuchiya-kai <yukaigangan@gmail.com>
2022-07-10 14:25:57 +0900

    基本的なCRAD機能を学習

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,12 +1,55 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { ItemsService } from './items.service';
+import type { Item } from './item.module';
 
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
   @Get()
-  findAll() {
+  findAll(): Item[] {
     return this.itemsService.findAll();
+  }
+
+  // pathパラメータを取る場合は@Param
+  @Get(':id')
+  findByID(@Param('id') id: string) {
+    return this.itemsService.findById(id);
+  }
+
+  // Request body を使用する場合は@bodyと書く
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item = {
+      id,
+      name,
+      price,
+      description,
+      status: 'ON_SALE',
+    } as const;
+    return this.itemsService.create(item);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string) {
+    return this.itemsService.update(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): void {
+    this.itemsService.delete(id);
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import type { Item } from './item.module';
 
 @Injectable()
 export class ItemsService {
-  findAll() {
-    return 'items service の返り値';
+  private items: Item[] = [];
+
+  findAll(): Item[] {
+    return this.items;
+  }
+
+  findById(id: string): Item | undefined {
+    const item = this.items.find((item) => item.id === id);
+    return item;
+  }
+
+  create(item: Item): Item {
+    this.items = [...this.items, item];
+    return item;
+  }
+
+  update(id: string): Item {
+    const item = this.findById(id);
+    item.status = 'SOLD_OUT';
+
+    return item;
+  }
+
+  delete(id: string): void {
+    this.items = this.items.filter((item) => item.id !== id);
   }
 }
